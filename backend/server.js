bc47c6a44ec43a5076d22f8b9bece1651fc02073
netanmangal <imnetanmangal@gmail.com>
2022-01-05 23:46:00 +0530

    error fixed

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require('morgan');
 const { CHAINS, NETWORKS, DENOMS } = require("@anchor-protocol/anchor-earn");
 
-const anchorEarn = require("./src/initiate-anchor");
+const { anchorEarn } = require("./src/initiate-anchor");
+const { anchorRoutes } = require("./src/routes/anchor");
 
 const app = express();
+app.use(morgan('tiny'));
 
 app.get("/", (req, res, next) => {
     res.status(200).send("We are on homepage");
-})
+});
 
-app.get("/getBalance", async (req, res, next) => {
-    try {
-        const balanceRes = await anchorEarn.anchorEarn.balance({ currencies: [DENOMS.UST, DENOMS.AUST] });
-        res.status(200).send({ status: true, balanceRes });
-    } catch (e) {
-        console.log(e);
-        res.status(500).send({status: false, msg: e});
-    }
-})
+app.use("/anchor", anchorRoutes);
 
 app.listen(3001, async () => {
     console.log(anchorEarn);
     console.log("Listening on port 3001");
-})
+});
