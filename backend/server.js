9ad6507e9c11aff0b9579d233efe42d28b0a2cb4
netanmangal <imnetanmangal@gmail.com>
2022-01-06 12:02:29 +0530

    secrets to config.jsonn

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,16 @@ const morgan = require('morgan');
 const { CHAINS, NETWORKS, DENOMS } = require("@anchor-protocol/anchor-earn");
 
 const { anchorEarn } = require("./src/initiate-anchor");
+const { rzpInstance } = require("./src/initiate-razorpay");
 const { anchorRoutes } = require("./src/routes/anchor");
+const { rzpRoutes } = require("./src/routes/razorpay");
 
 const app = express();
+
+app.use(express.json());        //for parsing application/json
+app.use(express.urlencoded({    //for parsing application/x-www-form-urlencoded
+    "extended": true
+}));
 app.use(morgan('tiny'));
 
 app.get("/", (req, res, next) => {
@@ -14,6 +21,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/anchor", anchorRoutes);
+app.use("/razorpay", rzpRoutes);
 
 app.listen(3001, async () => {
     console.log(anchorEarn);
--- backend/src/initiate-anchor.js
+++ backend/src/initiate-anchor.js
@@ -1,9 +1,10 @@
 const { AnchorEarn, CHAINS, NETWORKS } = require("@anchor-protocol/anchor-earn");
+const { mnemonic } = require("../config.json");
 
 const anchorEarn = new AnchorEarn({
     chain: CHAINS.TERRA,
     network: NETWORKS.BOMBAY_12,
-    mnemonic: 'glory insane swamp acquire cable baby ozone pull tissue awake crystal knee sketch kangaroo mirror tree bamboo invite lab over resource lion deal film',
+    mnemonic: mnemonic,
     address: 'terra1xmqftawktld0k0rvrxyapa4qkh58z790ug65td'
 });
 
